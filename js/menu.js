6aa0197c155101355965bf38123543dd7ce92bb7
retr0web <45012672+retr0web@users.noreply.github.com>
2019-05-08 11:25:37 +0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,14 @@
-// menu script
+const openMenu = document.getElementById('openMenu');
+const closeMenu = document.getElementById('closeMenu');
+const sideOver = document.getElementById('sideOver');
+
+openMenu.addEventListener('click', openOver);
+closeMenu.addEventListener('click', closeOver);
+
+function openOver(){
+    sideOver.style.width = '100%';
+}
+
+function closeOver(){
+    sideOver.style.width = '0%';
+}
