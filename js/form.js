027ce030bd853ae07629e8b3ac151213793eb9bb
retr0web <45012672+retr0web@users.noreply.github.com>
2019-06-03 20:03:43 +0300

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,7 @@
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click', message);
+
+function message() {
+    alert(`Ваше питання відправлено! \nЧекайте на відповідь.`)
+}
